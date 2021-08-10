3991ef3361280b67b7cacad8729a29ec6d34e4ca
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-09 19:43:43 -0600

    Create four New Screens

---
+++ src/screens/ColorsScreen.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { View, StyleSheet }  from "react-native"
+import { useTheme } from "@react-navigation/native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Colors, Strings } from "../values"
+
+export function ColorsScreen(){
+
+  const { container } = useTheme()
+
+  return (
+    <SafeAreaView style={container} edges={["bottom", "left", "right"]}>
+      <View style={styles.content} />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  content: {
+    justifyContent: "flex-start",
+  },
+})
+
+export function ColorsScreenOptions(){
+  return {
+      title: Strings.category_colors,
+      headerTintColor: Colors.white,
+      headerMode: "screen",
+      headerStyle: { 
+        backgroundColor: Colors.primary_color,
+      },
+  }
+}
--- src/values/styles.js
+++ src/values/styles.js
@@ -13,6 +13,7 @@ export const Styles = {
     },
     container: {
       flex: 1,
+      backgroundColor: Colors.tan_background,
     },
     categoryStyle: {
         width:"100%",

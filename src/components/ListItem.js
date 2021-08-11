cbb89cb1e96ab41bdcb9d20f6b82e38ad333fbfb
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-10 19:33:10 -0600

    Fix PhrasesActivity to Hide Images
    - Another way we could go about this is have the deafult value for mImageResourceId in the Word constructor to be -1 and have a public method called `hasImage()` to return true or false if the mImageResourceId property is -1
    - Then, in PhrasesScreen we can invoke that method hasImage() to check if the mImageResourceId is -1 or not to render the image or not
    
    This style we implemented in this commit, will return undefined if we did not set the image variable :) So this works as well

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,12 +3,9 @@ import { View, Text, Image }  from "react-native"
 
 /** {@link ListItem} will display list items for each {@link Word} in the list. */
 export function ListItem({ item }){
-    console.log(item.getImageResourceId)
     return (
         <View style={{ padding: 16, flex: 1, flexDirection: "row", alignItems:"center" }}>
-            <Image 
-                source={item.getImageResourceId}
-            />
+            {item.getImageResourceId && <Image source={item.getImageResourceId}/>}
             <View style={{ paddingLeft: 16 }}>
                 <Text>{item.getDefaultTranslation}</Text>
                 <Text>{item.getMiwokTranslation}</Text>

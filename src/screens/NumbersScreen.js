c5f06cf37f265eb8e24fca097a077452091e707a
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-11 07:44:31 -0600

    Play One Audio File
    add a method handleOnItemClick so that number_one.mp3 file is played when the user touches any list item

--- src/screens/NumbersScreen.js
+++ src/screens/NumbersScreen.js
@@ -1,6 +1,7 @@
-import React from "react"
-import { View, StyleSheet, FlatList }  from "react-native"
+import React, { useEffect, useState } from "react"
+import { View, StyleSheet, FlatList, Pressable, TouchableHighlight }  from "react-native"
 import { useTheme } from "@react-navigation/native"
+import { Audio } from "expo-av"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Colors, Dimens, Strings } from "../values"
 import Word from "../data/word"
@@ -9,6 +10,8 @@ import { ListItem } from "../components"
 export function NumbersScreen(){
 
   const { container } = useTheme()
+  const [sound, setSound] = useState()
+
   const words = []
   words.push(new Word("one", "lutti", require("../assets/images/drawable-mdpi/number_one.png")))
   words.push(new Word("two", "otiiko", require("../assets/images/drawable-mdpi/number_two.png")))
@@ -21,12 +24,31 @@ export function NumbersScreen(){
   words.push(new Word("nine", "wo’e", require("../assets/images/drawable-mdpi/number_nine.png")))
   words.push(new Word("ten", "na’aacha", require("../assets/images/drawable-mdpi/number_ten.png")))
 
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync() }
+      : undefined;
+  }, [sound])
+
+  const handleOnItemClick = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+        require("../assets/audio/number_one.mp3")
+    )
+    await sound.playAsync()
+  }
   return (
     <SafeAreaView style={container} edges={["bottom", "left", "right"]}>
       <View style={styles.content}>
         <FlatList 
           data={words}
-          renderItem={({ item }) => <ListItem item={item} style={{ backgroundColor: Colors.category_numbers }}/>}
+          renderItem={({ item }) => {
+            return (
+              <Pressable onPress={handleOnItemClick}>
+                <ListItem item={item} style={{ backgroundColor: Colors.category_numbers }} onPress={handleOnItemClick}/>
+              </Pressable>
+            )
+          }}
           getItemLayout={(_, index) => ({
             length: Dimens.list_item_height + Dimens.word_list_item_separator_height, 
             offset: Dimens.list_item_height + Dimens.word_list_item_separator_height * index,

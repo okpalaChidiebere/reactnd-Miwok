a4a705dd74a82a0d50b6265c28f81df6a695308a
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-13 12:38:42 -0600

    Measuring Category tabs views

--- src/components/TabView.js
+++ src/components/TabView.js
@@ -1,4 +1,4 @@
-import React, { useEffect, forwardRef } from "react"
+import React, { useEffect, useState, forwardRef, useRef } from "react"
 import { View, StyleSheet, Text } from "react-native"
 import tabScreens from "../utils/tabUtils"
 import { Colors } from "../values"
@@ -22,17 +22,38 @@ const TabIndicator = () => {
 }
 
 export function TabView(){
+    /**
+     * With measureLyout we need to specify the relative node in order to measure.
+     * It could be window, screen or a container. But in our case, its the TabView(that is a container to our CategoryTabs)
+     * This is why we created this ref that we will pass to the measureLayout for each 
+     * CategoryTab as we want to measure their positions
+     */
+    const tabViewRef = useRef()
+    const [measures, setMeasures] = useState([]) //stores measurements for out Tab items which we will pass to the TabIndicator
     useEffect(() => {
+        const m = [] //we will populate the temp array in our onSuccess callback
+
         /**
          * We want to measure the layout(x, y, height, width) for each CartegoryTab in relation to this component 
          * (the parent container for CartegoryTab) before this component(TabView) mounts
          */
         tabScreens.forEach(tabScreen => {
-            //tabScreen.ref.current.measureLayout()
+            tabScreen.ref.current.measureLayout(
+                tabViewRef.current,
+                //on measurement success callback function
+                (x, y, width, height) => {
+                    m.push({ x, y, width, height })
+
+                    //check if we are done getting measurement for each CategoryTabs
+                    if(m.length === tabScreens.length){
+                        setMeasures(m) //set the measures state with all our measurements
+                    }
+                },
+            )
         })
     }, [])
     return (
-        <View style={styles.tabView}>
+        <View ref={tabViewRef} style={styles.tabView}>
             {tabScreens.map(tabScreen => <CategoryTab key={tabScreen.key} tabScreen={tabScreen} ref={tabScreen.ref} />)}
             <TabIndicator />
         </View>
